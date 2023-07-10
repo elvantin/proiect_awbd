87c90fed5a346bb24736ecc08fb7a1d489227aac
elvantin <elvantin@gmail.com>
2023-07-10 22:08:05 +0300

    rating done

---
+++ src/main/java/com/example/awbd/model/forms/AudiotrackRating.java
@@ -0,0 +1,11 @@
+package com.example.awbd.model.forms;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class AddRating {
+    private Long idAudiotrack;
+    private int rating;
+}
